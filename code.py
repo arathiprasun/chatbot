6c4983fb29f7b68857b0d670d1a3eafe592dba83
arathiprasun <141842107+arathiprasun@users.noreply.github.com>
2023-08-11 14:53:11 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import streamlit as st
+
+st.set_page_config(
+   page_title="Cool App",
+   page_icon="🧊",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
 
 st.title("Echo Bot")
 
