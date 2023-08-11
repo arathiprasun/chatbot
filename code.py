0d5f2517f0868722db3a68d5c3e27cb27f85388c
arathiprasun <141842107+arathiprasun@users.noreply.github.com>
2023-08-11 16:57:25 +0100

    Update code.py

--- code.py
+++ code.py
@@ -35,3 +35,4 @@ if prompt := st.chat_input("What is up?"):
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
+    
