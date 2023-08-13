8bda29839638db3ccd626f4f9f6441d29c307256
arathiprasun <141842107+arathiprasun@users.noreply.github.com>
2023-08-13 15:45:06 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import random
 import time
+from google.cloud import storage
+import json
+from datetime import datetime
 
 st.title("Simple chat")
+
+# Initialize Google Cloud Storage client
+storage_client = storage.Client()
+bucket_name = 'your-bucket-name'
+object_prefix = 'chat-messages/'
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -39,5 +47,30 @@ if prompt := st.chat_input("What is up?"):
             # Add a blinking cursor to simulate typing
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
+
+        # Upload assistant response to Google Cloud Storage
+        user_id = 'assistant'
+        upload_message(user_id, full_response)
+        
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+# Upload a new chat message
+def upload_message(user_id, message):
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    object_name = f'{object_prefix}{user_id}_{timestamp}.json'
+
+    message_data = {
+        'user_id': user_id,
+        'message': message,
+        'timestamp': timestamp
+    }
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(object_name)
+    blob.upload_from_string(json.dumps(message_data))
+
+# Example usage
+user_id = '123'
+message = 'Hello, how are you?'
+upload_message(user_id, message)
